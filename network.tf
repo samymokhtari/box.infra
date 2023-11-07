2f0bc8f0b346017d9c9fa360aad4783f20657863
Samy Mokhtari <samy.mokhtari213@gmail.com>
2023-11-07 17:43:00 +0100

    fix vpc

--- network.tf
+++ network.tf
@@ -1,6 +1,7 @@
 resource "google_compute_network" "vpc_network" {
   project                 = var.project_id
   name                    = var.vpc_name
+  auto_create_subnetworks = false
 
   depends_on = [google_project_service.compute]
 }
