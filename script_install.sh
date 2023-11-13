0fca2cc6ad44e586bfef5ed8ea278a18a11a2d09
Samy Mokhtari <37446507+samymokhtari@users.noreply.github.com>
2023-11-13 10:05:07 +0100

    Script Install, CI/CD, Docker Compose (#3)
    * clean terraform config
    
    * Script install, CI/CD, Docker Compose

---
+++ script_install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Updating and upgrading the system..."
+sudo apt update -y
+sudo apt upgrade -y
+
+echo "Installing curl..."
+sudo apt install curl -y
+
+echo "Check if Docker is already installed..."
+if [ -f /tmp/docker_installed ]; then
+    echo "Docker is already installed!"
+    exit 0
+fi
+
+echo "Installing Docker..."
+
+{
+    sudo apt install apt-transport-https ca-certificates gnupg2 software-properties-common -y
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add 
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+    sudo apt update -y
+    sudo apt install docker-ce -y 
+} || {
+    echo "Error installing Docker! Exiting..."
+    exit 1
+}
+
+
+echo "Adding user to docker group..."
+sudo groupadd docker
+
+echo "Docker installed successfully!"dock
+
+echo "Create a file to confirm the installation..."
+touch /tmp/docker_installed
--- storage.tf
+++
@@ -1,7 +0,0 @@
-resource "google_storage_bucket" "default" {
-  name          = var.bucket_storage
-  location      = var.region
-  force_destroy = true
-
-  public_access_prevention = "enforced"
-}
